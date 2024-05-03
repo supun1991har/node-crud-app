fb0ef28219aa4fb73ac9fa1fa8d7c710061ad520
supun chathuranga <supunfit@gmail.com>
2024-05-03 16:35:53 -0700

    Add server filles

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(3001, () => {
+  console.log("Server is Running");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello from API Server Updated");
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://supunfit:124028T@bakenddb.6wxf2ah.mongodb.net/?retryWrites=true&w=majority&appName=BakendDB"
+  )
+  .then(() => {
+    console.log("connectd to database!");
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log("connection failed!");
+  });
